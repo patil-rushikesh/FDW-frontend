3fedcdc577286512645183c08ec5d345bcb55f08
avirajkale50 <avirajkale2004@gmail.com>
2025-02-09 10:56:58 +0530

    basic setup is done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-500'>
+        <h1 className='text-amber-400'>Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
